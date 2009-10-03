9c5c122335fe48eff97619308056207e027f2582
Max Lapan <max.lapan@gmail.com>
2009-10-03 19:42:29 +0400

    fetch quote finished

--- quotes.py
+++ quotes.py
@@ -6,12 +6,14 @@ from google.appengine.ext import db
 
 
 class Quote (db.Model):
+    symbol = db.StringProperty (required = True)
     date = db.DateProperty (required = True)
     open = db.FloatProperty (required = True)
     high = db.FloatProperty (required = True)
     low = db.FloatProperty (required = True)
     close = db.FloatProperty (required = True)
     volume = db.IntegerProperty (required = True)
+    period = db.StringProperty (required = True, choices = ['day', 'week', 'month'])
 
 
 def sourceInstance (source):
@@ -27,43 +29,58 @@ def sourceInstance (source):
 
 
 class Source:
-    def fetch (symbol, start, finish = datetime.date.today ()):
+    def fetch (symbol, start, finish = datetime.date.today (), period = 'day'):
         """
         fetches symbol's quotes and return array of quote object
         """
         return None
 
 
-
 # http://ichart.finance.yahoo.com/table.csv?s=MSFT&d=9&e=3&f=2009&g=d&a=2&b=13&c=1986&ignore=.csv
 class YahooSource (Source):
-    def fetch (self, symbol, start, finish = datetime.date.today ()):
-        req = '/table.csv?s=%(sym)s&d=%(to_m)d&e=%(to_d)d&f=%(to_y)d&g=d&a=%(from_m)d&b=%(from_d)d&c=%(from_y)d&ignore=.csv' % {
+    def fetch (self, symbol, start, finish = datetime.date.today (), period = 'day'):
+        req = '/table.csv?s=%(sym)s&d=%(to_m)d&e=%(to_d)d&f=%(to_y)d&g=%(period)s&a=%(from_m)d&b=%(from_d)d&c=%(from_y)d&ignore=.csv' % {
             'sym' : symbol,
             'to_m' : finish.month - 1,
             'to_d' : finish.day,
             'to_y' : finish.year,
             'from_m' : start.month - 1,
             'from_d' : start.day,
-            'from_y' : start.year }
+            'from_y' : start.year,
+            'period' : self.normalize_period (period)}
         conn = httplib.HTTPConnection ("ichart.finance.yahoo.com")
         conn.request ("GET", req)
         r = conn.getresponse ()
         res = r.read ()
         conn.close ()
         result = []
-        for l in res.split ("\n")[1:]:
+        lines = res.split ("\n")[1:]
+        lines.reverse ()
+        for l in lines:
             if l == '':
                 continue
             arr = l.split (',')
             date = arr[0].split ('-')
-            result.append (Quote (date = datetime.date (int (date[0]), int (date[1]), int (date[2])),
+            result.append (Quote (symbol = symbol,
+                                  date = datetime.date (int (date[0]), int (date[1]), int (date[2])),
                                   open = float (arr[1]),
                                   high = float (arr[2]),
                                   low  = float (arr[3]),
                                   close = float (arr[4]),
-                                  volume = long (arr[5])))
+                                  volume = long (arr[5]),
+                                  period = period))
         return result
+
+
+    def normalize_period (self, period):
+        if period == 'day':
+            return 'd'
+        elif period == 'week':
+            return 'w'
+        elif period == 'month':
+            return 'm'
+        else:
+            return 'd'
 
 
 class FinamSource (Source):
