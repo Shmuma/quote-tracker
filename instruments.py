fe03cdb83e85521d8f856d5ad85df911ddcb67a2
Max Lapan <max.lapan@gmail.com>
2009-10-03 14:27:54 +0400

    instrument list management

--- instruments.py
+++ instruments.py
@@ -3,6 +3,15 @@
 from google.appengine.ext import db
 
 
-class Instrument(db.Model):
+class Instrument (db.Model):
     symbol = db.StringProperty (required = True)
     source = db.StringProperty (required = True, choices = ['finam.rts'])
+
+    @classmethod
+    def by_symbol (cls, symbol):
+        q = Instrument.gql ("where symbol = :1", symbol)
+        res = q.fetch (1)
+        if len (res):
+            return res[0]
+        else:
+            return None
