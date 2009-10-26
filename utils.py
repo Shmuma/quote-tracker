883e7dc07ae6c0712e1c904bb56dc40d585951be
Max Lapan <max.lapan@gmail.com>
2009-10-26 18:55:15 +0300

    http interface for COT query

--- utils.py
+++ utils.py
@@ -7,4 +7,6 @@ def parseIsoDate (str):
     Converts string of format YYYY-MM-DD to datetime object
     """
     d = str.split ("-")
+    if len (d) != 3:
+        return None
     return datetime.date (int (d[0]), int (d[1]), int (d[2]))
