7ad76a2874df5ae7bff95607abf030981ef79207
Max Lapan <max.lapan@gmail.com>
2009-11-05 23:51:07 +0300

    download news data

--- news.py
+++ news.py
@@ -3,20 +3,23 @@
 # FX News tracker
 import datetime
 import httplib
-#import json
+import json
 from google.appengine.ext import db
 
 
 class News_Record (db.Model):
     id = db.IntegerProperty (required = True)
-    lang = db.StringProperty (required = True, choices = ["ru","en"])
     when = db.DateTimeProperty (required = True)
-    score = db.IntegerProperty (required = True) # Importance score 0-100
+    # Importance score 0-100
+    score = db.IntegerProperty (required = True)
     name = db.StringProperty (required = True)
-    country = db.StringProperty (required = True)
-    pred = db.StringProperty (required = True)
-    fore = db.StringProperty (required = True)
+    name_ru = db.StringProperty (required = False)
+    country = db.StringProperty (required = False)
+    pred = db.StringProperty (required = False)
+    fore = db.StringProperty (required = False)
     fact = db.StringProperty (required = False)
+    # Comma-separated list of currencies that can be affected. None if it will potentially affect all FX market.
+    curr = db.StringProperty (required = False)
 
     def exists (self):
         q = News_Record.gql ("WHERE id=:1", self.id)
@@ -27,7 +30,7 @@ def get_calendar_data (date):
     res = None
     conn = httplib.HTTPConnection ("fbs.com")
     try:
-        # Warning. fbs has a bug. When we request UTC time, it returns Moscow time. So, we request UTC+1, and remove 1 hour to get UTC.
+        # Warning: fbs has a bug. When we request UTC time, it returns Moscow time. So, we request UTC+1, and remove 1 hour to get UTC.
         req = "/ru/analytics/economic_calendar?action=calendar&day=%d&month=%d&year=%d&country=-1&timezone=1&lang=1&week=1" % (
             date.day, date.month, date.year)
         conn.request ("GET", req)
@@ -39,11 +42,48 @@ def get_calendar_data (date):
     
 
 
+def filter_str (str):
+    if str == '-':
+        return None
+    else:
+        return str
 
-def process_week (date = None):
+
+def parse_date_time (date, time):
+    return datetime.datetime.today ()
+
+
+
+def fetch_week (date = None):
     if date == None:
         date = datetime.date.today ()
     
     # Download json data from fbs.com
     data = get_calendar_data (date)
-    return data
+    if data == None:
+        return []
+    count = 0
+    res = []
+    rus = {}
+    for obj in json.read (data):
+        if obj['siteId'] == '2':
+            res.append (News_Record (id = int (obj['id']), when = parse_date_time (obj['date'], obj['time']), 
+                                     score = 0, name = obj['index'].encode ('utf-8'),
+                                     country = obj['country'], 
+                                     pred = filter_str (obj['pred']),
+                                     fore = filter_str (obj['forecast']),
+                                     fact = filter_str (obj['fact']),
+                                     curr = None));
+        else:
+            rus[int (obj['id'])] = obj['index'].encode ('utf-8')
+        count = count + 1
+
+    # update name_ru field of according objects
+    for obj in res:
+        if (obj.id+1 in rus) and (rus[obj.id+1].find (obj.name) == 0):
+            r = rus[obj.id + 1].split (' - ')
+            obj.name_ru = r[1]
+        elif (obj.id-1 in rus) and (rus[obj.id-1].find (obj.name) == 0):
+            r = rus[obj.id - 1].split (' - ')
+            obj.name_ru = r[1]
+    return res
